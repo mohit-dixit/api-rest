f683fb72b133de6406f7c5f02e25d906744c78ac
unknown <mohit.dixit354@gmail.com>
2025-05-17 16:19:07 +0530

    CORS Config added

--- src/main/java/com/alloymobile/restapi/resource/DepartmentResource.java
+++ src/main/java/com/alloymobile/restapi/resource/DepartmentResource.java
@@ -1,13 +1,8 @@
 package com.alloymobile.restapi.resource;
-
 import java.util.List;
-
 import org.springframework.web.bind.annotation.RestController;
-
 import com.alloymobile.restapi.persistence.Department;
 import com.alloymobile.restapi.service.DepartmentService;
-
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,7 +10,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
 
-@CrossOrigin(origins = { "http://localhost:4200" })
 @RestController
 public class DepartmentResource {
 
