97edcec621808509b9b40ba24a3cd7388e64fa83
unknown <mohit.dixit354@gmail.com>
2025-05-27 20:13:10 +0530

    Login and Users APIs are added

---
+++ src/main/java/com/alloymobile/restapi/resource/UsersResource.java
@@ -0,0 +1,51 @@
+package com.alloymobile.restapi.resource;
+import java.util.List;
+import org.springframework.web.bind.annotation.RestController;
+import com.alloymobile.restapi.persistence.Users;
+import com.alloymobile.restapi.service.UsersService;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+
+@RestController
+public class UsersResource {
+
+    UsersService service;
+
+    public UsersResource(UsersService service){
+        this.service = service;
+    }
+    
+    @GetMapping(value = "/users")    
+    public List<Users> getAll(){
+        return this.service.getAll();
+    }
+
+    @GetMapping(value = "/users/{id}")    
+    public Users getById(@PathVariable Long id){
+        return this.service.getById(id);
+    }
+
+    @PostMapping(value = "/signup")
+    public Users add(Users user) {
+        return this.service.add(user);
+    }
+
+    @PutMapping(value = "/users/{id}", consumes = "application/json")
+    public Users update(@PathVariable Long id, @RequestBody Users user) {
+        return this.service.update(id, user);
+    }
+
+    @DeleteMapping(value = "/users/{id}")
+    public void delete(@PathVariable Long id){
+        this.service.delete(id);
+    }
+
+    @PostMapping(value = "/login")
+    public Users add(Users user) {
+        return this.service.add(user);
+    }
+}
---
+++ src/main/java/com/alloymobile/restapi/service/UsersService.java
@@ -0,0 +1,48 @@
+package com.alloymobile.restapi.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.alloymobile.restapi.persistence.Users;
+import com.alloymobile.restapi.persistence.UsersRepository;
+
+@Service
+public class UsersService {
+
+    UsersRepository repository;
+
+    public UsersService(UsersRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Users> getAll(){
+        return this.repository.findAll();
+    }
+
+    public Users getById(Long id){
+        return this.repository.findById(id).get();
+    }
+
+    public Users add(Users user){
+        user.setRole("USER"); // Default role
+        user.setActive(true); // Default active status
+        return this.repository.save(user);
+    }
+
+    public Users update(Long id, Users user){
+        Optional<Users> usr = this.repository.findById(id);
+        if(usr.isPresent()){
+            usr.get().setUsername(user.getUsername());
+            usr.get().setEmail(user.getEmail());
+            usr.get().setPassword(user.getPassword());
+            return this.repository.save(usr.get());
+        }
+        throw new RuntimeException();
+    }
+
+    public void delete(Long id){
+        this.repository.deleteById(id);
+    }
+}
